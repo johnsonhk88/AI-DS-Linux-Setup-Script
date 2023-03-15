a2dfb0820e183dc6719ce6dabbd1f3d45b38f823
Johnson Chong <johnson@prodigyelectronics.com>
2023-03-15 11:19:48 +0800

    feat(add new docker enginer/compose install method4 for new version)

--- Docker-Engine-Install-remove/docker-compose-install-new2.sh
+++ Docker-Engine-Install-remove/docker-compose-install-new2.sh
@@ -1,5 +1,5 @@
 sudo apt update
 sudo apt upgrade
-curl -SL https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
