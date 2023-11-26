496964e850845700e3a8d9346a8570d3b6e1f0fd
Johnson Chong <johnsonhk88@gmail.com>
2023-11-26 22:51:18 +0800

    feat(add support LLM more library support)

--- Install-common-data-science-library/install-llm-transformer-hugging-face.sh
+++ Install-common-data-science-library/install-llm-transformer-hugging-face.sh
@@ -7,3 +7,6 @@ pip3 install accelerate # for pytorch distrution training boilerplate code neede
 pip3 install peft # for LLMs (Parameter-Efficient Fine-Tuning)
 pip3 install trl # for LLMs (Transformer Reinforcement Learning)
 pip3 install tiktoken # openAI project fast BPE tokeniser 
+pip3 install pytorch-pretrained-bert # BERT for pytorch 
+pip3 install torchdata # for NLP pyTorch
+pip3 install evaluate # for LLM evaluation
