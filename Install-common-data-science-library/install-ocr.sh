1b48a7098047d276fe2c69eee0c4ff0867d181e7
Johnson Chong <johnsonhk88@gmail.com>
2024-08-20 23:27:02 +0800

    feat(update tesseract ocr)

--- Install-common-data-science-library/install-ocr.sh
+++ Install-common-data-science-library/install-ocr.sh
@@ -1,6 +1,7 @@
 #for ubuntu linux
 sudo add-apt-repository ppa:alex-p/tesseract-ocr-devel
 sudo apt install -y tesseract-ocr
+sudo apt install -y libtesseract-dev
 
 #for 
 pip4 install -U git+https://github.com/madmaze/pytesseract.git
